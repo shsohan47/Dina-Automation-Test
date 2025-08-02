921b435016be198538b1ffb314a2278279408ac3
sohanpro <shsohan47@gmail.com>
2025-08-02 12:16:57 +0600

    🎨Doing until Edit Rundown Template

--- cypress/e2e/rundownV1View.cy.js
+++ cypress/e2e/rundownV1View.cy.js
@@ -19,6 +19,25 @@ describe("Rundown v1 View - All Test Cases",()=>
             const rundown = new BasicRundown();
             cy.visit('/')
             rundown.leftSideBarRundownClick();
+            //check the button open the rundown bar and click more option
+            cy.get('span[variant="h7"]')
+            .contains('Rundowns')
+            .should('exist')
+            .parentsUntil('body')
+            .find('button[aria-label="More"]')
+            .click()
+            //That should open the Edit temnplate container
+            cy.get(rundown.selectors.EditRundownTemplateContainer)
+            .should("exist")
+            .within(()=>
+            {
+                cy.contains('Edit Rundown Templates').click()
+            })
+            //That should open Rundown template container
+            cy.get('span[variant="h7"]')
+            .contains("Rundown Templates")
+            .should('exist')
+
         })
         
     })
--- cypress/support/pageObject/BasicRundown.js
+++ cypress/support/pageObject/BasicRundown.js
@@ -1,11 +1,13 @@
 export default class BasicRundown{
     selectors={
         LeftSideBarRoleTab:'button[role="tab"]',
+        EditRundownTemplateContainer:'[role="menu"][aria-orientation="vertical"][data-state="open"]'
     }
 
     leftSideBarRundownClick()
     {
        return cy.get(this.selectors.LeftSideBarRoleTab,{timeout:15000}).eq(8).click()
     }
+    
     
 }
