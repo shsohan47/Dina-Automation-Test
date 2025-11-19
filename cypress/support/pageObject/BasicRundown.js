e3ab06751a36433723500029018df028bf40595a
shsohan47 <shsohan47@gmail.com>
2025-11-19 16:40:48 +0600

    polishing with ai

--- cypress/support/pageObject/BasicRundown.js
+++ cypress/support/pageObject/BasicRundown.js
@@ -1,42 +1,63 @@
-export default class BasicRundown{
-    selectors={
-        LeftSideBarRoleTab:'button[role="tab"]',
-        EditRundownTemplateContainer:'[role="menu"][aria-orientation="vertical"][data-state="open"]',
-        CreateRundownTemplateDialog:'div[role="dialog"][data-state="open"]',
-     
+import BasePage from './BasePage';
+
+/**
+ * BasicRundown - Page Object for Rundown functionality
+ * @extends BasePage
+ */
+class BasicRundown extends BasePage {
+    /**
+     * Selectors for rundown elements
+     */
+    selectors = {
+        leftSideBarRoleTab: 'button[role="tab"]',
+        editRundownTemplateContainer: '[role="menu"][aria-orientation="vertical"][data-state="open"]',
+        createRundownTemplateDialog: 'div[role="dialog"][data-state="open"]',
+    };
+
+    /**
+     * Click on the Rundown tab in the left sidebar
+     * @returns {Cypress.Chainable}
+     */
+    leftSideBarRundownClick() {
+        return cy.get(this.selectors.leftSideBarRoleTab, { timeout: 15000 }).eq(8).click();
     }
 
-    leftSideBarRundownClick()
-    {
-       return cy.get(this.selectors.LeftSideBarRoleTab,{timeout:15000}).eq(8).click()
-    }
-
-    CreateNewGroupForRundownTemplate()
-    {
+    /**
+     * Create a new group for rundown template
+     * @returns {Cypress.Chainable}
+     * @example
+     * const rundown = new BasicRundown();
+     * rundown.CreateNewGroupForRundownTemplate();
+     */
+    CreateNewGroupForRundownTemplate() {
         // Open the Rundown template container
-       return cy.get('span[variant="h7"]')
-        .contains("Rundown Templates")
-        .should('exist')
-        //this will traverse up to the parent div container of the Rundown template section
-        .parents('div').eq(1)
-        .within(() => {
-            cy.wait(2000);
-        // Click "New Group" inside this container
-        cy.contains('button', 'New Group').should('be.visible').click();
+        return cy.get('span[variant="h7"]')
+            .contains('Rundown Templates')
+            .should('exist')
+            // This will traverse up to the parent div container of the Rundown template section
+            .parents('div').eq(1)
+            .within(() => {
 
-        // Wait for the new group to appear and rename it
-        cy.contains('Untitled', { timeout: 10000 })
-        .dblclick()
-        .type("Automated Rundown Template Group{enter}"); // press enter to confirm
+                // Click "New Group" inside this container
+                cy.contains('button', 'New Group').should('be.visible').dblclick();
 
-    });
-
+                // Wait for the new group to appear and rename it
+                cy.contains('Untitled', { timeout: 10000 })
+                    .should('be.visible') // Wait for visibility instead of hard wait
+                    .dblclick()
+                    .type('Automated Rundown Template Group{enter}'); // press enter to confirm
+            });
     }
-    getRundownCreateTemplateDialog()
-    {
-        return cy.contains("h2","Create Rundown Template").should('be.visible')
-                .parents(this.selectors.CreateRundownTemplateDialog);
+
+    /**
+     * Get the rundown create template dialog
+     * @returns {Cypress.Chainable}
+     */
+    getRundownCreateTemplateDialog() {
+        return cy.contains('h2', 'Create Rundown Template')
+            .should('be.visible')
+            .parents(this.selectors.createRundownTemplateDialog);
     }
-    
-    
 }
+
+export default BasicRundown;
---
+++ cypress/support/pageObject/RundownV1.js
@@ -0,0 +1,21 @@
+import BasePage from './BasePage';
+
+/**
+ * RundownV1 - Page Object for Rundown V1 specific functionality
+ * @extends BasePage
+ * 
+ * This is a placeholder for future Rundown V1 specific methods.
+ * Add methods here as you develop more rundown features.
+ */
+class RundownV1 extends BasePage {
+    /**
+     * Selectors for Rundown V1 elements
+     */
+    selectors = {
+        // Add your selectors here as you develop features
+    };
+
+    // Add your methods here
+}
+
+export default RundownV1;
